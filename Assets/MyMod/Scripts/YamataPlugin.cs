951db0a028db6e91ff2dfcf9ecaf2a668ce33ce5
Daemonarian <daemonarian@gmail.com>
2025-08-29 21:39:11 -0400

    rename all references to mod from 'My Mod' to 'Yamata'

--- Assets/MyMod/Scripts/YamataPlugin.cs
+++ Assets/MyMod/Scripts/YamataPlugin.cs
@@ -6,11 +6,11 @@ using BepInEx;
 namespace com.daemonarium.yamata
 {
     [BepInPlugin("com.daemonarium.yamata", "Yamata", "0.0.1")]
-    public class MyModPlugin : BaseUnityPlugin
+    public class YamataPlugin : BaseUnityPlugin
     {
         private void Awake()
         {
-            Logger.LogInfo("My Mod is awake!");
+            Logger.LogInfo("Yamata is awake!");
         }
     }
 }
