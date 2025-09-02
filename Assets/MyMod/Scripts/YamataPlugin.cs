5c53dee50e0198ed8e603a5d9e110287121c5743
Daemonarian <daemonarian@gmail.com>
2025-09-02 00:56:58 -0400

    mod now starts the game in the In-Between stage

--- Assets/MyMod/Scripts/YamataPlugin.cs
+++ Assets/MyMod/Scripts/YamataPlugin.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using BepInEx;
+using System.IO;
 
-namespace com.daemonarium.yamata
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace com.daemonarium.Yamata
 {
     [BepInPlugin("com.daemonarium.yamata", "Yamata", "0.0.1")]
     public class YamataPlugin : BaseUnityPlugin
@@ -11,6 +17,42 @@ namespace com.daemonarium.yamata
         private void Awake()
         {
             Logger.LogInfo("Yamata is awake!");
+            new Harmony("com.daemonarium.yamata").PatchAll();
+            Logger.LogInfo("Yamata finished patching.");
+        }
+    }
+
+    [HarmonyPatch(typeof(NetworkLevelLoader), "LoadLevel", new Type[]
+    {
+        typeof(int),
+        typeof(int),
+        typeof(float),
+        typeof(bool)
+    })]
+    public class NetworkLevelLoader_LoadLevel
+    {
+        internal static void Prefix(ref int _buildIndex)
+        {
+            try
+            {
+                CharacterSave chosenSave = SplitScreenManager.Instance.LocalPlayers[0].ChosenSave;
+                if (!((CharacterSaveData)chosenSave.PSave).NewSave && !string.IsNullOrEmpty(chosenSave.PSave.AreaName))
+                {
+                    return;
+                }
+                for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+                {
+                    string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+                    if (fileNameWithoutExtension != null && fileNameWithoutExtension == "DreamWorld")
+                    {
+                        _buildIndex = i;
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+            }
         }
     }
 }
